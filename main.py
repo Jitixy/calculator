616419b70116e8c51540e154d8a58573dcff8d6c
jitixy <daljeetsinghxy@gmail.com>
2023-03-15 23:44:17 -0400

    updated the code so that it gives a better interface and some more math functions.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Simple Calculator Program in Python
+# Simple Calculator Program in Python with a user-friendly interface
 
 def add(x, y):
     return x + y
@@ -16,29 +16,54 @@ def divide(x, y):
     return x / y
 
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+def exponent(x, y):
+    return x ** y
+
+
+def modulo(x, y):
+    return x % y
+
+
+def floor_divide(x, y):
+    return x // y
+
+
+print("Welcome to the Simple Calculator!")
+print("Please select the type of calculation you want to perform:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exponentiation")
+print("6. Modulo")
+print("7. Floor Division")
 
 # Take input from the user
-choice = input("Enter choice(1/2/3/4): ")
+choice = input("Enter your choice(1/2/3/4/5/6/7): ")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
+    print("The sum of", num1, "and", num2, "is", add(num1, num2))
 
 elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
+    print("The difference between", num1, "and", num2, "is", subtract(num1, num2))
 
 elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
+    print("The product of", num1, "and", num2, "is", multiply(num1, num2))
 
 elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
+    print("The quotient of", num1, "and", num2, "is", divide(num1, num2))
+
+elif choice == '5':
+    print(num1, "raised to the power of", num2, "is", exponent(num1, num2))
+
+elif choice == '6':
+    print("The remainder when", num1, "is divided by", num2, "is", modulo(num1, num2))
+
+elif choice == '7':
+    print("The floor division of", num1, "and", num2, "is", floor_divide(num1, num2))
 
 else:
     print("Invalid input")
